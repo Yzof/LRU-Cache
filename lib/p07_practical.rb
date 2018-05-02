4bf94ce1caaa148c78b9e0f7bf6fb21d2fbed9ef
Ozzy Paniagua <ozzy.coding@gmail.com>
2018-05-02 10:20:37 -0700

    P07 Complete! 100% Completion

--- lib/p07_practical.rb
+++ lib/p07_practical.rb
@@ -1,5 +1,22 @@
 require_relative 'p05_hash_map'
 
-def can_string_be_palindrome?(string) 
-  
+def can_string_be_palindrome?(string)
+  str_arr = string.split("")
+  mid = string.length / 2
+  left, right = [], []
+
+  if mid.odd?
+    str_arr.slice!(mid, 1)
+  end
+
+  left = str_arr.drop(mid).sort
+  right = str_arr.take(mid).sort
+
+  left.each_with_index do |char, idx|
+    if char != right[idx]
+      return false
+    end
+  end
+
+  true
 end
