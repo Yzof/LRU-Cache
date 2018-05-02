3833efa513878765ada7b6ac4a1dc7b3e1746c05
Ozzy Paniagua <ozzy.coding@gmail.com>
2018-05-01 20:43:45 -0700

    P02 Complete!

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,15 +4,26 @@ end
 
 class Array
   def hash
-    total = 1
-    self.each_value do |num, idx|
-
+    total = 0
+    self.each_with_index do |num, idx|
+      total += (idx + 1) * num
     end
+    total.hash
   end
 end
 
 class String
   def hash
+    arr = []
+
+    self.each_byte do |num|
+      if num === 0
+        next
+      end
+      arr.push(num)
+    end
+
+    arr.hash
   end
 end
 
@@ -20,7 +31,12 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
+    keys = self.keys.sort
+    keys = keys.each_with_index do |sym, idx|
+      hash = sym.to_s.hash
+      keys[idx] = hash
+    end
+    keys.hash
 
-    0
   end
 end
